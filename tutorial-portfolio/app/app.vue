368ce496dd9d65296b9900f1de340bdb3766f9b1
Ljaewon-123 <jml6388@naver.com>
2024-07-20 17:03:43 +0900

    basic nuxt-router

--- tutorial-portfolio/app/app.vue
+++ tutorial-portfolio/app/app.vue
@@ -8,4 +8,10 @@
 
 <script setup lang="ts">
 // const layout = 'another'
+// nuxtLayout에 :name추가하면 레이아웃 전역으로 사용
+
+// ***************
+// 처음만 서버를 통할 뿐이지 그 이후에는 CSR로 동작
+// ***************
+
 </script>
