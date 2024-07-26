bf271d2fa23f84e2c6d9f5ceab125d7a77ddf441
Ljaewon-123 <jml6388@naver.com>
2024-07-26 19:55:19 +0900

    여러가지 css 적용 , 정적파일 가져오는법
    setup, nuxt.config, assets/밑에 style파일

--- nuxt-tutorial/app/pages/index.vue
+++ nuxt-tutorial/app/pages/index.vue
@@ -1,10 +1,19 @@
 <template>
   <div>
-    Hello from the main Page!
+    <h1>
+      Hello from the main Page!
+    </h1>
+
+    <!-- 정적파일 public -->
+    <img src="/images/Logo_96.png">
+
+    <!-- 코드 외부에서 생성  -->
+    <img src="/assets/images/Logo_96.png">
   </div>
 </template>
 
 <script setup lang="ts">
+import '~/assets/styles/style.css' // 모든 파일에 전역 
 useSeoMeta({
   title: 'Main Page',
   description: 'This is the main page',
@@ -31,3 +40,8 @@ useHead({
   ]
 })
 </script>
+
+<style scoped>
+/* 해당 페이지에만 적용  */
+@import '~/assets/styles/style.css';
+</style>
--- nuxt-tutorial/nuxt.config.ts
+++ nuxt-tutorial/nuxt.config.ts
@@ -2,7 +2,14 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+
   future: {
     compatibilityVersion: 4
   },
+
+  css:[
+    '~/assets/styles/style.css'
+  ],
+
+  modules: ['@nuxtjs/tailwindcss']
 })
