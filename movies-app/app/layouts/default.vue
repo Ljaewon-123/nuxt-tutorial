7a179dde325e53b2ec2d003f060de1358f8515d0
Ljaewon-123 <jml6388@naver.com>
2024-07-28 21:03:58 +0900

    default setting  movies app

---
+++ movies-app/app/layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div  >
+
+
+    <main >
+      <slot></slot>
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  titleTemplate: '%s - Movies',
+  link: [
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.googleapis.com'
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap',
+      crossorigin: ''
+    }
+  ]
+})
+</script>
--- movies-app/nuxt.config.ts
+++ movies-app/nuxt.config.ts
@@ -1,5 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  future: {compatibilityVersion: 4},
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+  runtimeConfig:{
+    accessToken: process.env.accessToken
+  },
+  app:{
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: "https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.css"
+        }
+      ],
+      script: [
+        {
+          src:"https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.js"
+        }
+      ]
+    }
+  }
 })
