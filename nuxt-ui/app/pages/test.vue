2947524952a2c293b4b5160ccce80dc7c61f7475
Ljaewon-123 <jml6388@naver.com>
2024-12-13 17:32:11 +0900

    test backoff with useFetch

---
+++ nuxt-ui/app/pages/test.vue
@@ -0,0 +1,48 @@
+<template>
+<div>
+  <!-- <button  @click="refresh">backoff test</button> -->
+  <UButton @click="refresh" target="_blank" icon="i-simple-icons-x" color="gray" variant="ghost" >
+    backoff test
+  </UButton>
+</div>
+</template>
+
+<script setup lang="ts">
+
+const retryBackoff = (min = 1000, deadLine = 3) => {
+    let failCount = 0
+    let delay = min
+        
+    return {
+        backoff: () => {
+            failCount++
+            
+            if(failCount > deadLine) {
+                console.log('dead?')
+                throw Error('Server retry error')
+            }
+
+            delay = min * failCount
+
+            console.log(`Retry: ${failCount} in ${delay}ms`);
+
+            return delay
+        },
+        // 이거 필요없는거 같은데
+        reset: () => {
+            failCount = 0
+            delay = min
+        }
+    }
+}
+
+const { refresh } = useFetch('/api/backoff',{
+  method:'post',
+  server: false,
+  immediate: false,
+  onResponseError:() => {
+    
+  }
+})
+
+</script>
